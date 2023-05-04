adc30829a8559b1ef8c7b039ec4ca637816a8a79
Dmitriy <tsukikohaya@gmail.com>
2023-05-04 13:00:29 +0300

    fix: helper args

--- helpers/args.ts
+++ helpers/args.ts
@@ -4,15 +4,15 @@ interface IRes {
    t: undefined | string
 }
 
-export function getArgs(args: string[]) {
+export function getArgs(processArgs: string[]) {
    const res: IRes = {
       s: undefined,
       h: false,
       t: undefined
    }
-   const [exec, path, ...rest] = args
-   rest.forEach((el, idx, array) => {
-      if (rest.length > 1) {
+   const args = processArgs.slice(2)
+   args.forEach((el, idx, array) => {
+      if (array.length > 1) {
          if (el === '-s' && array[idx + 1][0] !== '-') {
             res.s = array[idx + 1]
          }
